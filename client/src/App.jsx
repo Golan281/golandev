00977f3d4034d5d373dda17aafd1c71184306dfb
Golan Blumenkrantz <golan281@gmail.com>
2023-06-23 20:14:45 +0300

    chore: update yml config & intro page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,14 +16,22 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Golan Blumenkrantz</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          linkedin
+        </button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          GitHub
+        </button>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> */}
+        <a href='https://golandev.tech/stocks/index.html'>getStocks</a>
+        <a href='https://golandev.tech/generador/generador.html'>generador</a>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
